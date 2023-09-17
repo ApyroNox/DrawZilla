9157acd808077820c434a9456f03abf76eb86a3f
ApyroNox <gkonto000@gmail.com>
2023-09-17 19:32:15 +0200

    used autoformatter

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
 var config = {
-	MediaObserver: {
-		'prefers-color-scheme': false,
-		orientation: true,
-		device: true,
-	},
-	ScalingDetails: {
-		reservedHeigth: 56,
-		reservedWidth: 0,
-		padding: 16,
-		aspectRatio: {
-			width: 4,
-			height: 3,
-		},
-	},
+  MediaObserver: {
+    'prefers-color-scheme': false,
+    orientation: true,
+    device: true,
+  },
+  ScalingDetails: {
+    reservedHeigth: 56,
+    reservedWidth: 0,
+    padding: 16,
+    aspectRatio: {
+      width: 4,
+      height: 3,
+    },
+  },
 };
 
 window.addEventListener('load', () => {});
